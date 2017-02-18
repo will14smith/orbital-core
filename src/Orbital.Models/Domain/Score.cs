3dd7f0a193b0e948af2fab29d77f895c5cf20f4f
Will Smith <will@toxon.co.uk>
2017-02-18 19:34:35 +0000

    Added Score & ScoreTarget models

---
+++ src/Orbital.Models/Domain/Score.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Orbital.Models.Domain
+{
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local", Justification = "Private setters are needed for serialisation")]
+    public class Score
+    {
+        // needed for deserialisation
+        public Score() { }
+
+        public Score(int id, Score score)
+            : this(
+                id: id,
+                score: score,
+                targets: score.Targets
+            )
+        {
+        }
+
+        public Score(int id, Score score, IReadOnlyCollection<ScoreTarget> targets)
+            : this(
+                id: id,
+
+                personId: score.PersonId,
+                clubId: score.ClubId,
+                bowstyle: score.Bowstyle,
+
+                competitionId: score.CompetitionId,
+                roundId: score.RoundId,
+
+                totalScore: score.TotalScore,
+                totalGolds: score.TotalGolds,
+                totalHits: score.TotalHits,
+
+                shotAt: score.ShotAt,
+                enteredAt: score.EnteredAt,
+
+                targets: targets
+            )
+        {
+        }
+
+        public Score(int id,
+            int personId, int clubId, Bowstyle bowstyle,
+            int? competitionId, int roundId,
+            decimal totalScore, decimal totalGolds, decimal totalHits,
+            DateTime shotAt, DateTime enteredAt,
+            IReadOnlyCollection<ScoreTarget> targets)
+        {
+            Id = id;
+
+            PersonId = personId;
+            ClubId = clubId;
+            Bowstyle = bowstyle;
+
+            CompetitionId = competitionId;
+            RoundId = roundId;
+
+            TotalScore = totalScore;
+            TotalGolds = totalGolds;
+            TotalHits = totalHits;
+
+            ShotAt = shotAt;
+            EnteredAt = enteredAt;
+
+            Targets = targets;
+        }
+
+        public int Id { get; private set; }
+
+        public int PersonId { get; private set; }
+        public int ClubId { get; private set; }
+        public Bowstyle Bowstyle { get; private set; }
+
+        public int? CompetitionId { get; private set; }
+        public int RoundId { get; private set; }
+
+        public decimal TotalScore { get; private set; }
+        public decimal TotalGolds { get; private set; }
+        public decimal TotalHits { get; private set; }
+
+        public DateTime ShotAt { get; private set; }
+        public DateTime EnteredAt { get; private set; }
+
+        public IReadOnlyCollection<ScoreTarget> Targets { get; private set; }
+    }
+}
---
+++ src/Orbital.Models/Domain/ScoreTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Orbital.Models.Domain
+{
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local", Justification = "Private setters are needed for serialisation")]
+    public class ScoreTarget
+    {
+        // needed for deserialisation
+        public ScoreTarget(){}
+
+        public ScoreTarget(int id, ScoreTarget scoreTarget)
+            : this(
+            id: id,
+            score: scoreTarget.Score,
+            golds: scoreTarget.Golds,
+            hits: scoreTarget.Hits
+            )
+        {
+        }
+        public ScoreTarget(int id, decimal score, decimal golds, decimal hits)
+        {
+            Id = id;
+            Score = score;
+            Golds = golds;
+            Hits = hits;
+        }
+
+
+        public int Id { get; private set; }
+
+        public decimal Score { get; private set; }
+        public decimal Golds { get; private set; }
+        public decimal Hits { get; private set; }
+    }
+}
