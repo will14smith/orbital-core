fa5d3901662e483c0331dea92b6992db5f801988
Will Smith <will@toxon.co.uk>
2017-02-18 19:36:22 +0000

    Added IScoreRepository

---
+++ src/Orbital.Models/Repositories/IScoreRepository.cs
@@ -0,0 +1,18 @@
+﻿using Orbital.Models.Domain;
+using System.Collections.Generic;
+
+namespace Orbital.Models.Repositories
+{
+    public interface IScoreRepository
+    {
+        IReadOnlyCollection<Score> GetAll();
+        IReadOnlyCollection<Score> GetAllByPersonId(int personId);
+        IReadOnlyCollection<Score> GetAllByRoundId(int roundId);
+        IReadOnlyCollection<Score> GetAllByCompetitionId(int competitionId);
+        Score GetById(int id);
+
+        Score Create(Score round);
+        Score Update(Score round);
+        bool Delete(Score round);
+    }
+}
