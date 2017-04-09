53db78a5f6dc59e302b30b7f36bb4175fa62c2b7
Will Smith <will@toxon.co.uk>
2017-04-09 20:21:35 +0100

    Added root api controller

---
+++ src/Orbital.Web/RootController.cs
@@ -0,0 +1,19 @@
+﻿using Halcyon.HAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Orbital.Web
+{
+    [Route("api")]
+    public class RootController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new HALResponse(null).AddLinks(new[]
+            {
+                new Link("clubs", Url.Action("Get", "Club")),
+                new Link("club", Url.Action("Get", "Club", new { id="{id}" })),
+            }));
+        }
+    }
+}
