9d6b9a2c4972dc9788628da3e8e97b3a20513715
Will Smith <will@toxon.co.uk>
2017-04-19 21:19:06 +0100

    Person badge holder sub resource

---
+++ src/Orbital.Web/People/BadgeHolders/PersonBadgeHolderController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Halcyon.HAL;
+using Microsoft.AspNetCore.Mvc;
+using Orbital.Web.BadgeHolders;
+using Orbital.Web.Helpers;
+
+namespace Orbital.Web.People.BadgeHolders
+{
+    public class PersonBadgeHolderController : Controller
+    {
+        private readonly IBadgeHolderService _badgeHolderService;
+
+        public PersonBadgeHolderController(IBadgeHolderService badgeHolderService)
+        {
+            _badgeHolderService = badgeHolderService;
+        }
+
+        [HttpGet("api/person/{personId}/badges")]
+        public IActionResult Get(int personId)
+        {
+            var holders = _badgeHolderService.GetAllByPersonId(personId);
+
+            return this.Paginate(holders.Select(ViewModelToResponse).ToList(), "badgeHolders");
+        }
+
+        private HALResponse ViewModelToResponse(BadgeHolderViewModel badgeHolder)
+        {
+            return new HALResponse(badgeHolder)
+                .AddLinks(new Link("self", Url.Action("Get", "BadgeHolder", new { id = badgeHolder.Id })));
+        }
+    }
+}
