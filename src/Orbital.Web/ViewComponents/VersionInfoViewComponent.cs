fbf761f8271599b2797779472b1319082bb62ed8
Will Smith <will@toxon.co.uk>
2017-07-23 13:01:31 +0100

    Created version info view component

---
+++ src/Orbital.Web/ViewComponents/VersionInfoViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orbital.Web.Models;
+
+namespace Orbital.Web.ViewComponents
+{
+    [ViewComponent(Name = "VersionInfo")]
+    public class VersionInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(VersionInfo versionInfo)
+        {
+            return View(versionInfo);
+        }
+    }
+}
